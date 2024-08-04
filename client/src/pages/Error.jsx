3bd0e95b343b19e4c6346094f2388d98e38457a2
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-08-04 17:50:38 +0500

    Done with register and login page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,19 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import errorImg from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
   console.log(error);
   return (
-    <div>
-      <h1>Error Page !!!</h1>
-      <Link to="/dashboard">back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <img src={errorImg} alt="Error-not-found" />
+        <h3>Ooh! page not found</h3>
+        <p>we can't seem to find to the page you looking for.</p>
+        <Link to="/dashboard">Back Home</Link>
+      </div>
+    </Wrapper>
   );
 };
 export default Error;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,55 @@
 import { Link } from "react-router-dom";
 
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow
+          type="text"
+          name="name"
+          labelText="Name"
+          defaultValue="Uzair"
+        />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="Last Name"
+          defaultValue="Khan"
+        />
+        <FormRow
+          type="text"
+          name="location"
+          labelText="Location"
+          defaultValue="Peshawar"
+        />
+        <FormRow
+          type="email"
+          name="email"
+          labelText="Email"
+          defaultValue="uzair@gmail.com"
+        />
+        <FormRow
+          type="password"
+          name="password"
+          labelText="Password"
+          defaultValue="secret123"
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member ?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
