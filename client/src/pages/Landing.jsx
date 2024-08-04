875a0d150dd627e3053e2530dc269e7f475e6b64
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-08-04 17:17:50 +0500

    Landing page completed

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,16 +1,37 @@
-import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <StyledButton>Click Me</StyledButton>
-    </div>
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            perspiciatis ex dicta voluptate, quia sequi rerum modi soluta labore
+            debitis! Ullam in et assumenda repellat ab, voluptatibus officiis
+            placeat soluta.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login
+          </Link>
+        </div>
+        <img src={main} alt="job-hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
-const StyledButton = styled.button`
-  background-color: red;
-  color: white;
-`;
 export default Landing;
