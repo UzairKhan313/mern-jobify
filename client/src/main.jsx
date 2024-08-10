a632203e21aff42e2395f67d3035dbba4ae4c270
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-08-10 11:48:03 +0500

    creating axios instunce and testint it

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import customFetch from "../../utils/customFetch.js";
+
+const data = await customFetch.get("/test");
+console.log(data);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
